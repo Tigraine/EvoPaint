d4f6afaff28c4137a0055c8d6cd92f3f3bf625a1
tam <tam@edu.uni-klu.ac.at>
2010-04-15 13:31:03 +0200

    you now paint while LMB is pressed and do not have to drag the mouse anymore

--- src/evopaint/gui/Showcase.java
+++ src/evopaint/gui/Showcase.java
@@ -26,6 +26,7 @@ import java.util.Observable;
 import java.util.Observer;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
+import javax.swing.Timer;
 import javax.swing.event.MouseInputListener;
 
 /**
@@ -53,6 +54,8 @@ public class Showcase extends WrappingScalableCanvas implements MouseInputListen
     private Point currentMouseDragPosition;
 
     private BrushIndicatorOverlay brushIndicatorOverlay;
+    private Timer paintingTimer;
+    private Painter painter;
 
     public Showcase(Configuration configuration, MainFrame mf, World world, Perception perception, CommandFactory commandFactory) {
         super(perception.getImage());
@@ -68,6 +71,10 @@ public class Showcase extends WrappingScalableCanvas implements MouseInputListen
 
         this.brushIndicatorOverlay = new BrushIndicatorOverlay(this,
                 new Rectangle(configuration.brush.size, configuration.brush.size));
+
+        this.painter = new Painter();
+        this.paintingTimer = new Timer(0, this.painter);
+        this.paintingTimer.setDelay(10);
 
         addMouseWheelListener(this);
         addMouseListener(this);
@@ -129,8 +136,8 @@ public class Showcase extends WrappingScalableCanvas implements MouseInputListen
         if (e.getButton() == MouseEvent.BUTTON1) {
             leftButtonPressed = true;
             if (mainFrame.getActiveTool() == PaintCommand.class) {
-                paintCommand.setLocation(transformToImageSpace(e.getPoint()));
-                paintCommand.execute();
+                painter.setLocation(e.getPoint());
+                paintingTimer.start();
             } else if (mainFrame.getActiveTool() == MoveCommand.class) {
                 moveCommand.setSource(e.getPoint());
                 //moveCommand.setScale(this.scale);
@@ -174,6 +181,7 @@ public class Showcase extends WrappingScalableCanvas implements MouseInputListen
     public void mouseReleased(MouseEvent e) {
         if (e.getButton() == MouseEvent.BUTTON1) {
             leftButtonPressed = false;
+            paintingTimer.stop();
             if (mainFrame.getActiveTool() == SelectCommand.class) {
                 this.isDrawingSelection = false;
                 // FIXME selectCommand.setLocation(e.getPoint(), scale);
@@ -188,10 +196,7 @@ public class Showcase extends WrappingScalableCanvas implements MouseInputListen
         if (leftButtonPressed == true) {
             // FIXME this.currentMouseDragPosition = SelectCommand.TranslatePointToScale(e.getPoint(), scale);
             if (mainFrame.getActiveTool() == PaintCommand.class) {
-                // TODO: paint pixels between mouse drags
-                // TODO: Maybe refactor state into PaintCommand
-                paintCommand.setLocation(transformToImageSpace(e.getPoint()));
-                paintCommand.execute();
+                painter.setLocation(e.getPoint());
             } else if (mainFrame.getActiveTool() == MoveCommand.class) {
                 moveCommand.setDestination(e.getPoint());
                 moveCommand.execute();
@@ -261,4 +266,96 @@ public class Showcase extends WrappingScalableCanvas implements MouseInputListen
             Logger.log.error("Selection from %s-%s to %s-%s", selection.getStartPoint().getX(), selection.getStartPoint().getY(), selection.getEndPoint().getX(), selection.getEndPoint().getY());
         }
     }
+
+    private class Painter implements ActionListener {
+        private Point location;
+
+        public Painter() {
+        }
+
+        public void setLocation(Point locationInUserSpace) {
+            this.location = transformToImageSpace(locationInUserSpace);
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            paintCommand.setLocation(location);
+            paintCommand.execute();
+        }
+    }
+
+    /* a nice idea, but it is slow since we have to draw not only the line, but the pixels into the world
+    private class ContinuousPainter implements ActionListener {
+        private Point location;
+        private Queue<Point> destinations;
+
+        public ContinuousPainter() {
+            this.destinations = new ArrayDeque<Point>();
+        }
+
+        public void setLocation(Point locationInUserSpace) {
+            this.location = transformToImageSpace(locationInUserSpace);
+            this.destinations.clear();
+        }
+
+        public void setDestination(Point destinationInUserSpace) {
+         this.destinations.add(transformToImageSpace(destinationInUserSpace));
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            Point destination = destinations.peek();
+            while (destination != null) {
+                if (destination.x != location.x) {
+                    double gradient = gradient(location, destination);
+                    if (gradient < 0.5) {
+                        if (destination.y != location.y) {
+                            if (destination.y > location.y) {
+                                location.y += gradient;
+                            } else {
+                                location.y -= gradient;
+                            }
+                        }
+                        if (destination.x > location.x) {
+                            location.x += 1;
+                        } else {
+                            location.x -= 1;
+                        }
+                    } else {
+                        if (destination.x > location.x) {
+                            location.x += gradient;
+                        } else {
+                            location.x -= gradient;
+                        }
+                        if (destination.y > location.y) {
+                            location.y += 1;
+                        } else {
+                            location.y -= 1;
+                        }
+                    }
+                    break;
+                }
+                else if (destination.y != location.y) {
+                    if (destination.y > location.y) {
+                        location.y += 1;
+                    } else {
+                        location.y -= 1;
+                    }
+                    break;
+                } else {
+                    destinations.remove();
+                    destination = destinations.peek();
+                }
+            }
+            paintCommand.setLocation(location);
+            paintCommand.execute();
+        }
+
+        private double gradient(Point location, Point destination) {
+            assert(location.x - destination.x != 0);
+            return ((double)Math.abs(destination.y - location.y)) /
+                    ((double)Math.abs(destination.x - location.x));
+        }
+
+    }
+   */
+    
 }
